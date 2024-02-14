6635e03e4387d5c703d9ecb94be14fa24bf54532
cwgreinketamu <cwgreinke@tamu.edu>
2024-02-14 16:54:44 -0600

    Bug Fixing task
    Also added task layer, which all task objects need to be placed on to render correctly

---
+++ Game_Jam_Spring_2024/Assets/Bug Fixing Assets/BugMoving.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugMoving : MonoBehaviour
+{
+    private float randX;
+    private float randY;
+    public float speed = 0.25f;
+    private int[] modifier = {-1,1}; //used to randomly set the sign of randX and randY
+    private GameObject bugSpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bugSpawner = GameObject.Find("BugSpawner");
+        randX = Random.Range(1.0f, 2.0f) * modifier[Random.Range(0, 2)]; //randX: [-2,-1]U[1,2]
+        randY = Random.Range(1.0f, 2.0f) * modifier[Random.Range(0, 2)]; //randY: [-2,-1]U[1,2]
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(randX*speed, randY*speed);
+        if (transform.position.x > 10 || transform.position.x < -10) //wraps around sides of screen
+        {
+            transform.position = new Vector3(-1*transform.position.x, transform.position.y, 0);
+        }
+        if (transform.position.y > 6 || transform.position.y < -6) //wraps around bottom/top of screen
+        {
+            transform.position = new Vector3(transform.position.x, -1 * transform.position.y, 0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        bugSpawner.GetComponent<BugSpawning>().KillBug();
+    }
+}
---
+++ Game_Jam_Spring_2024/Assets/Bug Fixing Assets/BugSpawning.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugSpawning : MonoBehaviour
+{
+    [SerializeField] private GameObject bugPrefab;
+    [SerializeField] private int bugsToSpawn = 3; // Maximum number of enemies to spawn
+    private int bugsKilled = 0;
+
+    private void Start()
+    {
+        for (int i = 0; i < bugsToSpawn; ++i)
+        {
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-4.5f, 4.5f), Random.Range(-2.5f, 2.5f), 0);
+            Instantiate(bugPrefab, randomSpawnPosition, Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+    
+    public void KillBug()
+    {
+        ++bugsKilled;
+        if (bugsKilled == bugsToSpawn)
+        {
+            Debug.Log("All bugs killed!");
+            //close task and return to game successfully here
+        }
+    }
+}
