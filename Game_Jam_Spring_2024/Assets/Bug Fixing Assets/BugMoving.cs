a342a0d846d3656c2ac77588b8bee255a2406118
zacharyshewbarttamu <zacharyshewbart@tamu.edu>
2024-04-10 15:53:02 -0500

    Bug and drawing game small fixes

--- Game_Jam_Spring_2024/Assets/Bug Fixing Assets/BugMoving.cs
+++ Game_Jam_Spring_2024/Assets/Bug Fixing Assets/BugMoving.cs
@@ -14,14 +14,17 @@ public class BugMoving : MonoBehaviour
     void Start()
     {
         bugSpawner = GameObject.Find("BugSpawner");
-        randX = Random.Range(1.0f, 2.0f) * modifier[Random.Range(0, 2)]; //randX: [-2,-1]U[1,2]
-        randY = Random.Range(1.0f, 2.0f) * modifier[Random.Range(0, 2)]; //randY: [-2,-1]U[1,2]
+        randX = Random.Range(0.01f, 1.0f) * modifier[Random.Range(0, 2)]; //randX: [-2,-1]U[1,2]
+        randY = Random.Range(0.01f, 1.0f) * modifier[Random.Range(0, 2)]; //randY: [-2,-1]U[1,2]
+        Vector3 rotate = new Vector3 (randX, randY).normalized;
+        float angle = Mathf.Rad2Deg * Mathf.Atan2(rotate.y, rotate.x);
+        transform.rotation = Quaternion.Euler(0,0,angle-90);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position += new Vector3(randX*speed, randY*speed);
+        transform.position += new Vector3(randX, randY)*speed;
         if (transform.position.x > 10 || transform.position.x < -10) //wraps around sides of screen
         {
             transform.position = new Vector3(-1*transform.position.x, transform.position.y, 0);
